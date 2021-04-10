ff272b96d63231b3a62d74089defb283dfeb1ea6
Natalia <natyap28@hotmail.com>
2021-04-09 22:46:08 -0300

    create card

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native'
+import { Image, View, Text, TouchableOpacity } from 'react-native'
 import styles from './styles'
 import { Feather } from '@expo/vector-icons'
+import Trash from '../../../assets/trash'
+import Eye from '../../../assets/eye'
 
 export default function home() {
   return (
     <View style={styles.container}>
+
       <View style={styles.header}>
         <Text style={styles.text}>Cartões</Text>
         <TouchableOpacity 
@@ -15,6 +18,20 @@ export default function home() {
           <Feather name="plus" size={22} color="#000" />
         </TouchableOpacity>
       </View>
+
+      <View style={styles.viewCard}>
+        <View style={styles.infoCardHeader}>
+          <Text style={styles.infoTextCard}>Nome do cartão</Text>
+          <Text style={styles.infoTextCard}>Bandeira</Text>
+        </View>
+
+        <View style={styles.infoCardFooter}>
+          <Text style={styles.cardName}>Nome Completo</Text>
+          <Text style={styles.cardNumber}>1234 1234 1234 1234</Text>
+        </View>
+      </View>
+
+      
     </View>
   )
 }
