6b7828b18964ad8ceb26020606a9c41094252419
Natalia <natyap28@hotmail.com>
2021-04-10 20:52:55 -0300

    add gradient color to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { View, Text } from "react-native";
 import styles from './CardStyle'
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Card() {
   return (
     <View style={styles.viewCard}>
+      <LinearGradient
+        colors={['rgba(236, 0, 140, 0.8)', 'rgba(252, 103, 103, 0.8)']}
+        start={ {x: 1, y: 0.8} }
+        style={styles.background}
+      />
       <View style={styles.infoCardHeader}>
         <Text style={styles.infoTextCard}>Nome do cartão</Text>
         <Text style={styles.infoTextCard}>Bandeira</Text>
