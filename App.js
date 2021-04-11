a16cdd90cf9927e3b70d0aec740b9bb970969d4b
Natalia <natyap28@hotmail.com>
2021-04-11 02:21:28 -0300

    add Roboto font

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Routes from './src/routes'
-  import { useFonts, Raleway_400Regular, Raleway_500Medium, Raleway_600SemiBold, Raleway_700Bold } from '@expo-google-fonts/raleway'
+import { useFonts, Raleway_400Regular, Raleway_500Medium, Raleway_600SemiBold, Raleway_700Bold, } from '@expo-google-fonts/raleway'
+import { Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 export default function App() {
   let [fontsLoaded] = useFonts({
     Raleway_400Regular,
     Raleway_500Medium,
     Raleway_600SemiBold,
-    Raleway_700Bold
+    Raleway_700Bold,
+    Roboto_700Bold
   })
 
   if (!fontsLoaded) {
