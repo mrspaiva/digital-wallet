b4fbf9c40fce7e14d73c39316dd12d061b257989
Natalia <natyap28@hotmail.com>
2021-04-12 17:59:00 -0300

    catch input values

--- src/pages/newCard/index.js
+++ src/pages/newCard/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native'
@@ -7,6 +7,10 @@ import Card from '../../components/Card'
 
 
 export default function newCard() {
+  const [cardName, setCardName] = useState('')
+  const [name, setName] = useState('')
+  const [number, setNumber] = useState('')
+
   const navigation = useNavigation()
   
   function navigationToNewCard() {
@@ -16,23 +20,40 @@ export default function newCard() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Adicionar cartão</Text>
-      <Card />
+      <Card 
+      cardNameValue={cardName}
+      nameValue={name}
+      numberValue={number}
+      />
 
       <Text style={styles.label}>NOME DO CARTÃO</Text>
-      <TextInput style={styles.input} placeholder="Nome do cartão"/>
+      <TextInput 
+      style={styles.input} 
+      placeholder="Nome do cartão"
+      value={cardName}
+      onChangeText={cardName => setCardName(cardName)}
+      />
 
       <Text style={styles.label}>NOME COMPLETO</Text>
-      <TextInput style={styles.input} placeholder="Nome Completo"/>
+      <TextInput 
+      style={styles.input} 
+      placeholder="Nome Completo"
+      value={name}
+      onChangeText={name => setName(name)}
+      />
 
       <Text style={styles.label}>NÚMERO</Text>
       <TextInput 
       style={styles.input} 
       placeholder="1234 1234 1234 1234"
       keyboardType="numeric" 
+      value={number}
+      onChangeText={number => setNumber(number)}
       />
 
       <TouchableOpacity 
       style={styles.addButton}
+      title='submit'
       onPress={navigationToNewCard}
       >
         <Text style={styles.addButtonText}>ADICIONAR</Text>
