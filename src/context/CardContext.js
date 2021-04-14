e55695e1a93b17991429520ebb0eb4f6f29dca05
Natalia <natyap28@hotmail.com>
2021-04-14 00:40:31 -0300

    create card context

---
+++ src/context/CardContext.js
@@ -0,0 +1,27 @@
+import React, { createContext, useState } from 'react'
+
+export const CardContext = createContext()
+
+export default function CardProvider({children}) {
+  const [cards, setCards] = useState([])
+  const [cardName, setCardName] = useState('')
+  const [name, setName] = useState('')
+  const [number, setNumber] = useState('')
+
+  
+  return (
+    <CardContext.Provider value={{
+      addCard, 
+      deleteCard, 
+      cards, 
+      cardName,
+      setCardName,
+      name,
+      setName,
+      number,
+      setNumber
+      }}>
+      {children}
+    </CardContext.Provider>
+  )
+}
